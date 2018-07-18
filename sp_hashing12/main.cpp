74a90b2d0a1ea48b7d9ac86de25f3683d74b49e4
Deepalaxmi <khdeepalaxmi@gmail.com>
2018-07-18 11:04:09 +0530

    hashing

--- sp_hashing12/main.cpp
+++ sp_hashing12/main.cpp
@@ -11,7 +11,71 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void minIndexedChar(string s, string p){
+    map<char,int>m;
+    for (int j = 0; j < p.length(); j++) {
+        m[p[j]] = -1;
+    }
+    
+    for (int i = 0; i < s.length(); i++) {
+        if ( m.find(s[i]) != m.end()) {
+            if (m[s[i]] == -1) {
+                m[s[i]] = i;
+            }
+        }
+        
+    }
+    int minIndx = INT_MAX;
+    char minChar = '\0';
+    for (int j = 0; j < p.length(); j++) {
+        if (m[p[j]] != -1) {
+            if (m[p[j]] < minIndx ) {
+                minIndx = m[p[j]];
+                minChar = p[j];
+            }
+        }
+    }
+    if (minIndx != INT_MAX && minChar != '\0') {
+        cout << minChar;
+    }else{
+        cout << "No character present";
+    }
+}
+
+
+//2nd
+void minIndexedChar2(string s, string p){
+    map<char, int>m;
+    
+    for (int i = 0; i < p.length(); i++) {
+        m[p[i]]++;
+    }
+    
+    bool check = false;
+    for (int j = 0; j < s.length(); j++) {
+        if (m[s[j]]) {
+            check = true;
+            cout << s[j];
+            break;
+        }
+    }
+    
+    if (!check) {
+        cout << "No character present";
+    }
+    
+    
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-2    return 0;
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        string str, pat;
+        cin >> str >> pat;
+        minIndexedChar2(str, pat);
+        cout << endl;
+    }
+   return 0;
 }
